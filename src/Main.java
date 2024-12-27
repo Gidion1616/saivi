cc662b36bd7de21609659f7ff58034892dbab6d1
Gidion0606 <ggidion897@gmail.com>
2024-12-27 01:03:26 -0800

    new commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner
+public class Main {
+    public static void main(String[] args) {
+        Scanner me = new Scanner(System.in);
+
+        System.out.print("Hello and welcome!");
+    }
+}
